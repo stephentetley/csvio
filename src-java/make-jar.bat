f59a88596257fc7121ba56c0ead87c89472dd36c
stephentetley <stephen.tetley@gmail.com>
2022-08-29 21:30:30 +0100

    Updated dependencies. Added an Ant build file for the Java sources.

--- src-java/make-jar.bat
+++ src-java/make-jar.bat
@@ -1,9 +1,5 @@
 
 set %JAVA_HOME%="C:\Program Files\Zulu\zulu-11"
 
-javac -cp "../lib/commons-csv-1.9.0.jar;../lib/commons-io-2.11.0.jar" -Xlint:deprecation -d ./build  flixspt/csvio/*.java
-
-cd build 
-
-jar cf csviojava-1.6.jar ./flixspt/csvio/*.class
+ant
 
